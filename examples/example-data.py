01d77d43c6f209ce394fac4d7dda077994bc1590
Juan Nunez-Iglesias <jni@fastmail.com>
2024-03-06 21:28:22 +1100

    Specify plugin when opening

--- examples/example-data.py
+++ examples/example-data.py
@@ -3,7 +3,9 @@ import napari
 viewer = napari.Viewer()
 shear_layer, grains_layer = viewer.open(
         '/Users/jni/Dropbox/Dongchen Shared Juan developing script/'
-        'Shared with Juan/step21.defdap.yml')
+        'Shared with Juan/step21.defdap.yml',
+        plugin='napari-defdap'
+        )
 
 dock_widget, widget = viewer.window.add_plugin_dock_widget('napari-defdap')
 
