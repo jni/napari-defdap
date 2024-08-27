df6a9933aa67de0007ab139a8f6a9d784562155a
Juan Nunez-Iglesias <jni@fastmail.com>
2024-08-27 20:52:03 +0200

    Add widget for setting focus on specific track

---
+++ src/napari_defdap/_track_focus.py
@@ -0,0 +1,25 @@
+from magicgui import magic_factory
+
+
+@magic_factory(auto_call=True)
+def set_track_focus(
+        viewer: 'napari.viewer.Viewer',
+        layer: 'napari.layers.Tracks',
+        track_id: int,
+        ):
+    """Given a tracks layer and a track id, set camera focus on that track."""
+    df = layer.features  # make sure to add the dataframe to the tracks layer
+    track_locations = df[df['track_id' == track_id], ['t', 'y', 'x']]
+    if track_locations.shape[0] == 0:
+        return
+    current_timepoint = viewer.dims.point[0]
+    if current_timepoint not in track_locations.t:
+        current_timepoint = track_locations.t.iloc[-1]
+    new_point = track_locations[
+            track_locations.t == current_timepoint
+            ].to_numpy()
+    viewer.dims.point = new_point
+    if viewer.dims.ndisplay == 2:
+        viewer.camera.center = new_point[1:]
+    else:
+        viewer.camera.center = new_point
