1776baa5a02368a6a5c84d476f624dacf4064fc5
Juan Nunez-Iglesias <jni@fastmail.com>
2024-11-12 21:32:02 -0600

    Update widget example for non-timeseries and phase map

--- examples/example-data.py
+++ examples/example-data.py
@@ -1,7 +1,7 @@
 import napari
 
 viewer = napari.Viewer()
-shear_layer, grains_layer = viewer.open(
+shear_layer, grains_layer, phase_layer = viewer.open(
         '/Users/jni/Dropbox/Dongchen Shared Juan developing script/'
         'Shared with Juan/step21.defdap.yml',
         plugin='napari-defdap'
@@ -9,11 +9,13 @@ shear_layer, grains_layer = viewer.open(
 
 dock_widget, widget = viewer.window.add_plugin_dock_widget('napari-defdap')
 
+phase_layer.visible = False
+
 grains_layer.selected_label = 64
 grains_layer.mode = 'pick'
 
-dicmap = shear_layer.metadata['dicmap']
-ebsdmap = shear_layer.metadata['ebsdmap']
+dicmap = shear_layer.metadata['dicmap'][()]
+ebsdmap = shear_layer.metadata['ebsdmap'][()]
 
 g = dicmap.grains[63]
 eg = g.ebsd_grain
